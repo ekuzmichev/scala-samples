e1e6efc69a866b76afef8c0c51426a316add2c8b
ekuzmichev <evgenii.e.kuzmichev@gmail.com>
2021-12-14 10:22:38 +0300

    add TestControl execute test

--- cats-effect/src/test/scala/ru/ekuzmichev/ce/ExampleCatsEffectTestkit.scala
+++ cats-effect/src/test/scala/ru/ekuzmichev/ce/ExampleCatsEffectTestkit.scala
@@ -1,7 +1,11 @@
 package ru.ekuzmichev.ce
 
+import cats.Id
 import cats.effect.IO
 import cats.effect.std.Random
+import cats.effect.Outcome
+import cats.implicits._
+
 import scala.concurrent.duration._
 
 object ExampleCatsEffectTestkit {
@@ -38,5 +42,34 @@ class Suite extends CatsEffectSuite {
     val program = Random.scalaUtilRandom[IO].flatMap(retry(action, 1.minute, 5, _))
 
     TestControl.executeEmbed(program).assertEquals("success!")
+  }
+
+  test("backoff appropriately between attempts") {
+    case object TestException extends RuntimeException
+
+    val action               = IO.raiseError(TestException)
+    val program: IO[Nothing] = Random.scalaUtilRandom[IO].flatMap(retry(action, 1.minute, 5, _))
+
+    val result: IO[TestControl[Nothing]] = TestControl.execute(program)
+
+    result.flatMap { control =>
+      for {
+        _ <- control.results.assertEquals(None)
+        _ <- control.tick
+
+        _ <- 0.until(4).toList.traverse { i =>
+              for {
+                _ <- control.results.assertEquals(None)
+
+                interval <- control.nextInterval
+                _        <- IO(assert(interval >= 0.nanos))
+                _        <- IO(assert(interval < (1 << i).minute))
+                _        <- control.advanceAndTick(interval)
+              } yield ()
+            }
+
+        _ <- control.results.assertEquals(Some(Outcome.errored[Id, Throwable, Nothing](TestException)))
+      } yield ()
+    }
   }
 }
