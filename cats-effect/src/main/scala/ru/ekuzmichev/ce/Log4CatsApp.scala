e65cc9bff76218d7782a45d6b3af6dd9f7d7f55a
ekuzmichev <evgenii.e.kuzmichev@gmail.com>
2022-04-11 23:50:48 +0300

    Comment code

--- cats-effect/src/main/scala/ru/ekuzmichev/ce/Log4CatsApp.scala
+++ cats-effect/src/main/scala/ru/ekuzmichev/ce/Log4CatsApp.scala
@@ -1,24 +1,24 @@
-package ru.ekuzmichev.ce
-
-import cats.effect.{ IO, IOApp, Sync }
-import cats.implicits._
-import io.chrisdavenport.log4cats.slf4j.Slf4jLogger
-
-// No support for cats effect 3
-// java.lang.NoSuchMethodError: io.chrisdavenport.log4cats.slf4j.Slf4jLogger$.getLoggerFromSlf4j(Lorg/slf4j/Logger;Lcats/effect/kernel/Sync;)Lio/chrisdavenport/log4cats/SelfAwareStructuredLogger;
-object Log4CatsApp extends IOApp.Simple {
-  override def run: IO[Unit] =
-    for {
-      _ <- doSmth[IO]()
-    } yield ()
-
-  def doSmth[F[_]: Sync](): F[Unit] =
-    for {
-      logger <- Slf4jLogger.create[F]
-      _      <- logger.info(s"Starting...")
-      _ <- Sync[F].delay(logger.info("I could do anything")).onError {
-            case e => logger.error(e)("Something Went Wrong in safelyDoThings")
-          }
-      _ <- logger.info(s"Exiting...")
-    } yield ()
-}
+//package ru.ekuzmichev.ce
+//
+//import cats.effect.{ IO, IOApp, Sync }
+//import cats.implicits._
+//import io.chrisdavenport.log4cats.slf4j.Slf4jLogger
+//
+//// No support for cats effect 3
+//// java.lang.NoSuchMethodError: io.chrisdavenport.log4cats.slf4j.Slf4jLogger$.getLoggerFromSlf4j(Lorg/slf4j/Logger;Lcats/effect/kernel/Sync;)Lio/chrisdavenport/log4cats/SelfAwareStructuredLogger;
+//object Log4CatsApp extends IOApp.Simple {
+//  override def run: IO[Unit] =
+//    for {
+//      _ <- doSmth[IO]()
+//    } yield ()
+//
+//  def doSmth[F[_]: Sync](): F[Unit] =
+//    for {
+//      logger <- Slf4jLogger.create[F]
+//      _      <- logger.info(s"Starting...")
+//      _ <- Sync[F].delay(logger.info("I could do anything")).onError {
+//            case e => logger.error(e)("Something Went Wrong in safelyDoThings")
+//          }
+//      _ <- logger.info(s"Exiting...")
+//    } yield ()
+//}
