92eefe181e022a122dbac8810124073425aba72c
ekuzmichev <evgenii.e.kuzmichev@gmail.com>
2024-11-20 11:07:58 +0300

    Add zio aspect sample apps

--- zio2-samples/src/main/scala/ru/ekuzmichev/zio2/ZioAspectPlayground.scala
+++ zio2-samples/src/main/scala/ru/ekuzmichev/zio2/ZioAspectPlayground.scala
@@ -2,7 +2,18 @@ package ru.ekuzmichev.zio2
 
 import zio._
 
-object ZioAspectApp extends ZIOAppDefault {
+object ZioAspectSimpleApp extends ZIOAppDefault {
   override def run: ZIO[Any with ZIOAppArgs with Scope, Any, Any] =
     ZIO.attempt("Hello!") @@ ZIOAspect.debug
+}
+
+object ZioAspectCompositionApp extends ZIOAppDefault {
+  def download(url: String): ZIO[Any, Throwable, Chunk[Byte]] = ZIO.succeed(Chunk.empty)
+
+  override def run: ZIO[Any with ZIOAppArgs with Scope, Any, Any] =
+    ZIO.foreachPar(List("zio.dev", "google.com")) { url =>
+      download(url) @@
+        ZIOAspect.retry(Schedule.fibonacci(1.seconds)) @@
+        ZIOAspect.loggedWith[Chunk[Byte]](file => s"Downloaded $url file with size of ${file.length} bytes")
+    }
 }
